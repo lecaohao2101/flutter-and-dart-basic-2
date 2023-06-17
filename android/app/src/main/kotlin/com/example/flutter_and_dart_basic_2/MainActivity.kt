5c171c6349805be1c6292f682d2fcc365ca309de
Lê Cao Hào <lecaohao2101@gmail.com>
2023-06-17 20:40:41 +0700

    Creating a Widget

---
+++ android/app/src/main/kotlin/com/example/flutter_and_dart_basic_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_and_dart_basic_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_and_dart_basic_2/start_screen.dart';
+
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
